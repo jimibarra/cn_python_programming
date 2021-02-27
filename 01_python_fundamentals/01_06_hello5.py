9948cb480370858b4221ab241fa3f0f96d63673b
jimibarra <jimibarra18@hotmail.com>
2021-02-27 19:28:48 +0000

    Lab 01_python_fundamentals

--- 01_python_fundamentals/01_06_hello5.py
+++ 01_python_fundamentals/01_06_hello5.py
@@ -4,3 +4,6 @@ Write the necessary code to display "Hello World!" 5 times.
 
 
 '''
+
+message = "Hello World!"
+print(5*message)
--- 01_python_fundamentals/01_07_area_perimeter.py
+++ 01_python_fundamentals/01_07_area_perimeter.py
@@ -3,3 +3,8 @@
 Write the necessary code to display the area and perimeter of a rectangle that has a width of 2.4 and a height of 6.4.
 
 '''
+
+area = 6.4 * 2.4
+perimeter = 2 * (6.4 + 2.4)
+print(f"The area of the rectangle is {area}")
+print(f"The perimeter of the rectangle is {perimeter}")
